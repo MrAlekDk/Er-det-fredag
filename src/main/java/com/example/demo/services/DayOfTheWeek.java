82df68233e4febf0def0602aadc1bf688f9007fd
Alexander <alex030t@stud.kea.dk>
2021-02-08 11:18:22 +0100

    Hurtig fix: Der var skrevet 5 i stedet for 6 i "DayOfTheWeek" class. Calendar starter på 1, som svarer til Søndag

--- src/main/java/com/example/demo/services/DayOfTheWeek.java
+++ src/main/java/com/example/demo/services/DayOfTheWeek.java
@@ -9,7 +9,7 @@ public class DayOfTheWeek {
         Calendar calendar = Calendar.getInstance();
         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
-        if (dayOfWeek == 5) {
+        if (dayOfWeek == 6) {
             return true;
         } else {
             return false;
